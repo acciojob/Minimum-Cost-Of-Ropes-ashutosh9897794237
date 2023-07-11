66d4ca948bf2e1311f7bc7ee629eddb06579c000
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-11 21:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ function calculateMinCost(event) {
 		arr.sort(function (a,b) {return a-b})
 	}
   //your code here
+	import heapq
+def ConnectRope(ropes):
+    heapq.heapify(ropes)
+    res = 0
+    while len(ropes) > 1:
+        cur = heapq.heappop(ropes)+heapq.heappop(ropes)
+        heapq.heappush(ropes, cur)
+        res += cur
+    return res
+
+ropes = [8, 4, 6, 12]
+print(ConnectRope(ropes))
   document.getElementById("result").innerHTML = cost
   
 }  
